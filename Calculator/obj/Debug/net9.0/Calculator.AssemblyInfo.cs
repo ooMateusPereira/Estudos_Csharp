161ebb258e7f7f15f57e8a223ce44d611af98807
Mateus Pereira da Silva <mateusufc1@live.com>
2025-07-17 09:42:43 -0300

    init:

--- Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3309f5eff9331684ffb8f876591cdcc0fb2155e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0c144d67b9e9c001071faef4d1d3096be1bab8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
